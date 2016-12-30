9b5c00ff7ebfcaefb205aad42f37c7a40214d60c
Head_on_a_Stick <matthew.t.hoare@gmail.com>
2016-12-30 17:19:47 +0000

    Added colour numbers and commented out clear to preserve /etc/{issue,motd}

--- colourtty.sh
+++ colourtty.sh
@@ -1,19 +1,19 @@
 if [ "$TERM" = "linux" ]; then
-    printf "\e]PBfff796" # S_base00
-    printf "\e]PAc5f779" # S_base01
-    printf "\e]P0000000" # S_base02
-    printf "\e]P6418179" # S_cyan
-    printf "\e]P8888888" # S_base03
-    printf "\e]P2aece92" # S_green
-    printf "\e]P5963c59" # S_magenta
-    printf "\e]P19e1828" # S_red
-    printf "\e]PC4186be" # S_base0
-    printf "\e]PE71bebe" # S_base1
-    printf "\e]P9cf6171" # S_orange
-    printf "\e]P7bebebe" # S_base2
-    printf "\e]P4414171" # S_blue
-    printf "\e]P3968a38" # S_yellow
-    printf "\e]PFffffff" # S_base3
-    printf "\e]PDcf9ebe" # S_violet
-    clear # against bg artifacts
+    printf "\e]P0000000" # color0
+    printf "\e]P19e1828" # color1
+    printf "\e]P2aece92" # color2
+    printf "\e]P3968a38" # color3
+    printf "\e]P4414171" # color4
+    printf "\e]P5963c59" # color5
+    printf "\e]P6418179" # color6
+    printf "\e]P7bebebe" # color7
+    printf "\e]P8888888" # color8
+    printf "\e]P9cf6171" # color9
+    printf "\e]PAc5f779" # color10
+    printf "\e]PBfff796" # color11
+    printf "\e]PC4186be" # color12
+    printf "\e]PDcf9ebe" # color13
+    printf "\e]PE71bebe" # color14
+    printf "\e]PFffffff" # color15
+#   clear # removes artefacts but also removes /etc/{issue,motd}
 fi
